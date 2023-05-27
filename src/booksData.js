d564b74c96338bff24528d53c18319e99f8818d2
karineagredo <karina.a182@gmail.com>
2023-05-27 10:39:32 +1000

    first commit

---
+++ src/booksData.js
@@ -0,0 +1,17 @@
+const firstBook = {
+  id: 1,
+  author: 'Jordan Mot',
+  title: 'Last is best',
+  img: 'https://m.media-amazon.com/images/P/B005MJFA2W.01._SCLZZZZZZZ_SX500_.jpg',
+  ranking: 1,
+};
+const secondBook = {
+  id: 2,
+  author: 'Karine Agredo',
+  title: 'I got this',
+  img: 'https://m.media-amazon.com/images/P/B005MJFA2W.01._SCLZZZZZZZ_SX500_.jpg',
+  ranking: 3,
+};
+const Books = [firstBook, secondBook];
+
+export default Books;
---
+++ src/index.js
@@ -0,0 +1,33 @@
+import React from 'react';
+import reactDOM from 'react-dom/client';
+import './index.css';
+import Books from './booksData';
+
+const BookList = () => {
+  const getBook = (id) => {
+    const foundBook = Books.find((book) => book.id === id);
+    console.log(foundBook);
+  };
+  return (
+    <section className='bookList'>
+      {Books.map((book) => {
+        return <Book {...book} onBookClick={getBook} key={book.id} />;
+      })}
+    </section>
+  );
+};
+
+const Book = ({ img, id, title, author, ranking, onBookClick }) => {
+  return (
+    <article className='book'>
+      <div>{ranking}</div>
+      <img src={img} alt='' />
+      <h2>{title}</h2>
+      <h4>{author}</h4>
+      <button onClick={() => onBookClick(id)}>click me</button>
+    </article>
+  );
+};
+
+const root = reactDOM.createRoot(document.getElementById('root'));
+root.render(<BookList />);
